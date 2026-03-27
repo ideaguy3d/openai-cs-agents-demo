0c0623e387284b4f9fcd01fac3d46da8476fd5f4
Cal Geek <javascript.uiux@gmail.com>
2026-03-27 11:17:40 -0700

    Create example_run_app.sh

---
+++ python-backend/example_run_app.sh
@@ -0,0 +1,3 @@
+cd /Users/neo_collection1/Documents/GitHub/openai-cs-agents-demo/python-backend
+export OPENAI_API_KEY=YOUR_KEY
+python3 -m flask --app main:app run --host 0.0.0.0 --port 8000 --debug
